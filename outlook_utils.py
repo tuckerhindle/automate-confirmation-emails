f517428ccc9f9ca5b7a8eba00259bebdaeaa2bac
Tucker Hindle <tucker.hindle@gmail.com>
2023-02-15 15:57:07 -0500

    Support sending from shared mailbox

--- outlook_utils.py
+++ outlook_utils.py
@@ -13,21 +13,24 @@ def write_email_content(name, term, course, section):
 
     greeting = f"Hello {name},<br><br>"
     message = f"Thank you for completing the {term} Research Courses form.  The instructor approved your request for <b>{course} at {section}</b>.<br><br>Once FAU registration opens, please add this class to your schedule by following these instructions: <a href='https://www.fau.edu/registrar/registration/#Register'>FAU Guide to Register for Classes</a><br><br>"
-    signature = f"We are excited to see you this {term}!<br><br>--<br><b>FAU Lab Schools Research Team</b><br>Florida Atlantic University<br>777 Glades Road, Building 26F, Room 109<br>Boca Raton, Florida 33431"
+    signature = f"We are excited to see you this {term}!<br><br>--<br><b>FAU Lab Schools Research Team</b><br>Florida Atlantic University<br>777 Glades Road, Building 26F, Room 109<br>Boca Raton, Florida 33431<br><br>Email: <a href='mailto:fauhsresearch@fau.edu'>fauhsresearch@fau.edu</a><br>Social Media: <a href='https://canvas.fau.edu/courses/36616'>@fauhs_research</a><br>Website: <a href='https://fauhigh.fau.edu/student-research/publications-presentations-patents'>FAUHS Student Research Dashboard</a>"
 
     body = "<h3 style='font-weight:normal;'>" + greeting + message + signature + "</h3>"
 
     return subject, body
 
 
-def send_new_email(subject, recipients, body):
+def send_new_email(from_account, recipients, subject, body):
     """Sends a new email using MS Outlook to the specified recipients containing the subject line and body provided."""
     
     outlook = win32.Dispatch("outlook.application")
+    account = outlook.Session.Accounts.Item(from_account)
+
     mail = outlook.CreateItem(0)
 
-    mail.Subject = subject
+    mail._oleobj_.Invoke(*(64209, 0, 8, 0, account)) # send from specified account
     mail.To = recipients
+    mail.Subject = subject
     mail.HTMLBody = body
 
     mail.Send()
